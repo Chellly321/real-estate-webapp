0202d79236268466f0d37f4e92f58c4b4fc20590
Chellly321 <michelle.royer@icloud.com>
2024-01-04 21:58:26 +0100

    begin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 import Home from "./pages-user/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <>
+      <Router>
+        <Navbar />
+
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,38 @@
-const Navbar = () => {
-  return <div></div>;
-};
+import "./../styles/components/Navbar.css";
+import Logo from "../assets/logo/5.png";
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <NavLink to="/" className="nav-item">
+            Home
+          </NavLink>
+          <NavLink to="/properties" className="nav-item">
+            Properties
+          </NavLink>
+          <NavLink to="/projects" className="nav-item">
+            Projects
+          </NavLink>
+        </div>
+
+        <Link to="/">
+          <img src={Logo} className="logo" alt="Anra Caribbean Logo" />
+        </Link>
+
+        <div className="navbar-container">
+          <NavLink to="/about" className="nav-item">
+            About
+          </NavLink>
+          <NavLink to="/contact" className="nav-item">
+            Contact
+          </NavLink>
+        </div>
+      </nav>
+    </>
+  );
+}
 export default Navbar;
