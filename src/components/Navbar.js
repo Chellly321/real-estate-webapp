a7856a1a69f6dd0e54a1d4fac26a9cb1afb7ed9d
Chellly321 <michelle.royer@icloud.com>
2024-01-06 12:58:34 +0100

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,35 @@
 import "./../styles/components/Navbar.css";
 import Logo from "../assets/logo/5.png";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { NavLink, Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { click } from "@testing-library/user-event/dist/click";
 
 function Navbar() {
+  const [showLogo, setShowLogo] = useState(true);
+  const [showMobileLogo, setShowMobileLogo] = useState(true);
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <>
       <nav className="navbar">
-        <div className="navbar-container">
+        {showLogo && (
+          <Link to="/">
+            <img src={Logo} className="logo" alt="Anra Caribbean Logo" />
+          </Link>
+        )}
+
+        <div
+          className={
+            showMenu ? "navbar-container mobile-menu" : "navbar-container"
+          }
+        >
+          {showMobileLogo && (
+            <Link to="/">
+              <img src={Logo} className="menu-logo" alt="Anra Caribbean Logo" />
+            </Link>
+          )}
           <NavLink to="/" className="nav-item">
             Home
           </NavLink>
@@ -17,13 +39,6 @@ function Navbar() {
           <NavLink to="/projects" className="nav-item">
             Projects
           </NavLink>
-        </div>
-
-        <Link to="/">
-          <img src={Logo} className="logo" alt="Anra Caribbean Logo" />
-        </Link>
-
-        <div className="navbar-container">
           <NavLink to="/about" className="nav-item">
             About
           </NavLink>
@@ -31,6 +46,12 @@ function Navbar() {
             Contact
           </NavLink>
         </div>
+
+        <FontAwesomeIcon
+          className="fa-bars"
+          icon={faBars}
+          onClick={() => setShowMenu(!showMenu)}
+        />
       </nav>
     </>
   );
