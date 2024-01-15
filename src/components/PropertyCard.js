f93f481f990ea1a86f1f36fa173fe123d727c2d6
Chellly321 <michelle.royer@icloud.com>
2024-01-15 22:20:19 +0100

    card-property1

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/components/PropertyCard.css";
 
 function PropertyCard({ data }) {
   return (
@@ -8,37 +9,43 @@ function PropertyCard({ data }) {
       {data.length > 0 &&
         data.map((data) => {
           return (
-            <div className="card-container">
-              <div className="card">
-                <div className="card-top">
-                  <div className="tags">
-                    <span className="tag-type">huur</span>
-                    <span className="tag-location">Simpson Bay</span>
+            <Link to={`/properties/${data.id}`} key={data.id}>
+              <div className="card-container">
+                <div className="card">
+                  <div className="card-top">
+                    <div className="tags">
+                      <span className="tag-type">{data.listingType}</span>
+                      <span className="tag-location">{data.area}</span>
+                    </div>
+                    <img
+                      className="card_image"
+                      src={data.image}
+                      alt="property"
+                    />
                   </div>
-                  <img src={data.image} />
-                </div>
-                <div className="card-content">
-                  <div className="card-adress">{data.address}</div>
-                  <div className="card-price">{data.price}</div>
-                </div>
-                <div className="card-footer">
-                  <ul className="footer-items">
-                    <li className="list-item">
-                      <span>X</span>
-                      <span>{data.surface}</span>
-                    </li>
-                    <li className="list-item">
-                      <span>X</span>
-                      <span>{data.bedrooms}</span>
-                    </li>
-                    <li className="list-item">
-                      <span>X</span>
-                      <span>{data.furnished}</span>
-                    </li>
-                  </ul>
+                  <div className="card-content">
+                    <div className="card-adress">{data.address}</div>
+                    <div className="card-price">{data.price}</div>
+                  </div>
+                  <div className="card-footer">
+                    <ul className="footer-items">
+                      <li className="list-item">
+                        <span>X</span>
+                        <span>{data.surface}</span>
+                      </li>
+                      <li className="list-item">
+                        <span>X</span>
+                        <span>{data.bedrooms}</span>
+                      </li>
+                      <li className="list-item">
+                        <span>X</span>
+                        <span>{data.furnished}</span>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
-            </div>
+            </Link>
           );
         })}
     </>
